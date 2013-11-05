cdf3da91793f138a1bcbb0eb6fefcef7c4cc034f
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2013-11-05 17:23:14 -0600

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,8 @@ import sys
 from setuptools import setup, Extension
 from setuptools.dist import Distribution
 
-# auto-install cython
-Distribution(dict(setup_requires='Cython'))
-
-# check cython is installed
-try:
-    from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
-except ImportError:
-    print("Could not import Cython. Install `cython` and rerun.")
-    sys.exit(1)
+requires = ['Cython']
+packages = ['cyglfw3']
 
 # set the GLFW compiler flags
 extra_compile_args = []
@@ -28,16 +20,34 @@ if 'darwin' in platform:
     # homebrew calls it libglfw3
     glwf_lib = 'glfw3'
 
-
 ext_modules = [
-    Extension('cyglfw3.glfw3', ['cyglfw3/glfw3.pyx'], libraries=[glwf_lib],
+    Extension('cyglfw3.glfw3', 
+        ['cyglfw3/glfw3.pyx'], 
+        libraries=[glwf_lib],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args
     )
 ]
 ext_modules = cythonize(ext_modules)
 
-setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules,
+setup_dict = dict(
+    name="cyglfw3",
+    version='0.0.1',
+    description="Python bindings for GLFW 3+ using Cython",
+    author="Adam Griffiths",
+    author_email="adam.lw.griffiths@gmail.com",
+    install_requires=requires,
+    url='http://github.com/adamlwgriffiths/cyglfw3',
+    packages=packages,
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+    classifiers=(
+        'Development Status :: 2 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: ??',
+        'Programming Language :: Python',
+    ),
 )
+
+setup(**setup_dict)
