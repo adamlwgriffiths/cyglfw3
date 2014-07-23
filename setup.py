abcceddd96c7ffb2932457fc9563c778e68e4a81
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-07-23 10:45:19 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open('README.rst') as f:
 
 setup(
     name="cyglfw3",
-    version='0.0.4',
+    version='0.0.5',
     description="Python bindings for GLFW 3+ using Cython",
     long_description=long_description,
     license = 'BSD',
