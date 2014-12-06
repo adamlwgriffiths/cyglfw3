975169b347627d1fe7f2ade3a8c6e57c4045a1e0
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-12-06 13:13:59 +1100

    Fix import not being relative

--- cyglfw3/__init__.py
+++ cyglfw3/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 # the version of software
 # this is used by the setup.py script
-from version import __version__
+from .version import __version__
 
 from .glfw3 import *
 
